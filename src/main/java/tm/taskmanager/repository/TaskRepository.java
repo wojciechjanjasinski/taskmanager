9004dfc0e993a351227b2f80330666b47b0c2ade
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-09-06 11:23:48 +0200

    Small change in persistence.xml file.

--- src/main/java/tm/taskmanager/repository/TaskRepository.java
+++ src/main/java/tm/taskmanager/repository/TaskRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    
     List<Task> findTasksByQuit_OpenedMatchesAndOrderByDeadlineDesc();
     List<Task> findTasksByQuit_TerminatedMatchesAndOrderByDeadlineDesc();
 }
